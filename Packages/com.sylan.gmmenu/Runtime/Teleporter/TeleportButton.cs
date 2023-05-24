3dfdcf3ec95cfaab0cba110d274c4a52386b8412
Sylan <lauranemeth11@hotmail.com>
2023-05-24 02:12:57 -0400

    Teleport Button Editor Script

--- Packages/com.sylan.gmmenu/Runtime/Teleporter/TeleportButton.cs
+++ Packages/com.sylan.gmmenu/Runtime/Teleporter/TeleportButton.cs
@@ -1,23 +1,19 @@
-﻿
-using UdonSharp;
+﻿using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
-using VRC.Udon;
 using JetBrains.Annotations;
+using UnityEditor;
+using VRC.SDKBase.Editor.BuildPipeline;
+using Sylan.GMMenu;
 
 namespace Sylan.GMMenu
 {
     [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
     public class TeleportButton : UdonSharpBehaviour
     {
-        [NotNull,SerializeField] Transform GMMenu;
-        [NotNull] Teleporter teleporter;
+        [NotNull, SerializeField, HideInInspector] public Teleporter teleporter;
         public Transform teleportPosition;
         public VRC_SceneDescriptor.SpawnOrientation teleportOrientation;
-        private void Start()
-        {
-            teleporter = Utils.Modules.Teleporter(GMMenu.transform);
-        }
         public override void Interact()
         {
             VRCPlayerApi player = Networking.LocalPlayer;
@@ -25,3 +21,159 @@ namespace Sylan.GMMenu
         }
     }
 }
+
+#if !COMPILER_UDONSHARP && UNITY_EDITOR
+[InitializeOnLoad]
+public class DoorSyncInitialize : Editor, IVRCSDKBuildRequestedCallback
+{
+    private static bool SetSerializedProperties()
+    {
+        //Object with Serialized Property(s)
+        if (!GetSerializedObjects<TeleportButton>(out SerializedObject[] serializedObject)) return false;
+        foreach (var obj in serializedObject)
+        {
+            PopulateSerializedProperty<Teleporter>(obj, nameof(TeleportButton.teleporter));
+        }
+        //Set Serialized Property
+        return true;
+    }
+    //
+    //Run On Play
+    //
+    static DoorSyncInitialize()
+    //Rename Static Constructor to match Class name
+    {
+        EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+    }
+    private static void OnPlayModeStateChanged(PlayModeStateChange state)
+    {
+        if (state != PlayModeStateChange.ExitingEditMode) return;
+        SetSerializedProperties();
+    }
+    //
+    // Run On Build
+    //
+    public int callbackOrder => 0;
+
+    public bool OnBuildRequested(VRCSDKRequestedBuildType requestedBuildType)
+    {
+        if (requestedBuildType != VRCSDKRequestedBuildType.Scene) return false;
+        return SetSerializedProperties();
+    }
+
+    /// <summary>
+    /// Find exactly one object of Type T in the scene hierarchy.
+    /// </summary>
+    /// <typeparam name="T">Type of Object to get</typeparam>
+    /// <param name="obj">Local variable to contain the object</param>
+    /// <returns>True if successful, false if unsuccessful</returns>
+    public static bool GetObject<T>(out T obj) where T : MonoBehaviour
+    {
+        T[] objects = UnityEngine.Object.FindObjectsOfType<T>();
+
+        if (objects.Length == 0)
+        {
+            Debug.Log("[EditorUtilities] No Objects of type " + typeof(T).ToString());
+            obj = null;
+            return true;
+        }
+        if (objects.Length > 1)
+        {
+            Debug.LogError("[EditorUtilities] More than one object of type " + typeof(T).ToString());
+            obj = null;
+            return false;
+        }
+        obj = objects[0];
+        return true;
+    }
+    public static bool GetObjects<T>(out T[] obj) where T : MonoBehaviour
+    {
+        T[] objects = UnityEngine.Object.FindObjectsOfType<T>();
+        obj = objects;
+        return true;
+    }
+    /// <summary>
+    /// Set Serialized Property of Type T. Property must not be an array. 
+    /// </summary>
+    /// <typeparam name="T">Type of Object to set</typeparam>
+    /// <param name="serializedObject">Object with the property</param>
+    /// <param name="propertyName">Name of Serialized Property</param>
+    public static void PopulateSerializedProperty<T>(SerializedObject serializedObject, string propertyName) where T : MonoBehaviour
+    {
+        if (serializedObject == null) return;
+        SerializedProperty property;
+        property = serializedObject.FindProperty(propertyName);
+
+        // Get one matching components in the scene
+        GetObject<T>(out T obj);
+        property.objectReferenceValue = obj;
+
+        // Apply the changes to the component
+        serializedObject.ApplyModifiedProperties();
+    }
+    /// <summary>
+    /// Set Serialized Property of Type T. Property must be an array, and will be filled with all the objects found.
+    /// </summary>
+    /// <typeparam name="T">Type of Object to set</typeparam>
+    /// <param name="serializedObject">Object with the property</param>
+    /// <param name="propertyName">Name of Serialized Property</param>
+    public static void PopulateSerializedArray<T>(SerializedObject serializedObject, string propertyName) where T : MonoBehaviour
+    {
+        if (serializedObject == null) return;
+        SerializedProperty arrayProperty;
+        arrayProperty = serializedObject.FindProperty(propertyName);
+
+        // Get all the matching components in the scene
+        GetObjects<T>(out T[] objects);
+
+        // Assign the serialized references to the array
+        arrayProperty.ClearArray();
+        arrayProperty.arraySize = objects.Length;
+        for (int i = 0; i < objects.Length; i++)
+        {
+            arrayProperty.GetArrayElementAtIndex(i).objectReferenceValue = objects[i];
+        }
+        // Apply the changes to the component
+        serializedObject.ApplyModifiedProperties();
+    }
+    /// <summary>
+    /// Find an object of Type T in the scene hierarchy as a SerializedObject.
+    /// </summary>
+    /// <typeparam name="T">Type of Object to get</typeparam>
+    /// <param name="serializedObject">Local variable to contain the object</param>
+    /// <returns>True if successful, false if unsuccessful</returns>
+    public static bool GetSerializedObject<T>(out SerializedObject serializedObject) where T : MonoBehaviour
+    {
+        serializedObject = null;
+        if (!GetObject(out T obj))
+        {
+            EditorApplication.isPlaying = false;
+            return false;
+        }
+        if (obj != null) serializedObject = new SerializedObject(obj);
+        return true;
+    }
+    /// <summary>
+    /// Find objects of Type T in the scene hierarchy as an array of type SerializedObject.
+    /// </summary>
+    /// <typeparam name="T">Type of Object to get</typeparam>
+    /// <param name="serializedObjects">Local variable to contain the objects</param>
+    /// <returns>True if successful, false if unsuccessful</returns>
+    public static bool GetSerializedObjects<T>(out SerializedObject[] serializedObjects) where T : MonoBehaviour
+    {
+        serializedObjects = null;
+        if (!GetObjects(out T[] obj))
+        {
+            EditorApplication.isPlaying = false;
+            return false;
+        }
+        serializedObjects = new SerializedObject[obj.Length];
+        for (int i = 0; i < obj.Length; i++)
+        {
+            serializedObjects[i] = new SerializedObject(obj[i]);
+        }
+        return true;
+    }
+}
+
+#endif
