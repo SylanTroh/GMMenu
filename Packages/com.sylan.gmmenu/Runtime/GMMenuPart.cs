1831c4c6fda0cbb2af08c59656f6253b569ffb88
JanSharp <steinmeister36@gmail.com>
2025-07-12 19:06:47 +0200

    Fix grammar

--- Packages/com.sylan.gmmenu/Runtime/GMMenuPart.cs
+++ Packages/com.sylan.gmmenu/Runtime/GMMenuPart.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
 public class GMMenuPart : UdonSharpBehaviour
 {
-    // Using a 'using Sylan.GMMenu;' made UdonSharp fail to compile, claiming that GMMenu is a namespace that
+    // Using 'using Sylan.GMMenu;' made UdonSharp fail to compile, claiming that GMMenu is a namespace that
     // is being used as a type, but only if it was compiling without the audio manager package present.
     // That makes just about zero sense but it is what it is.
     [HideInInspector, SerializeField] public Sylan.GMMenu.GMMenu gmMenu;
